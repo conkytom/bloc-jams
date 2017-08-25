5d9989b62093193b99933288c33bc26243c74a47
Thomas Conkwright <tmconkwright@gmail.com>
2017-08-24 21:02:11 -0400

    Assignment 20 completed

--- scripts/album.js
+++ scripts/album.js
@@ -56,7 +56,7 @@ var createSongRow = function(songNumber, songName, songLength){
                 $(this).html(playButtonTemplate);
                 $('.main-controls .play-pause').html(playerBarPlayButton);
                 currentSoundFile.pause();
-                currentlyPlayingSongNumber = null;
+//                currentlyPlayingSongNumber = null;
             }
         }
     };
@@ -151,6 +151,20 @@ var updatePlayerBarSong = function (){
     $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
+var togglePlayFromPlayerBar = function(){
+    var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+    if (currentSoundFile.isPaused()) {
+        $('.main-controls .play-pause').html(playerBarPauseButton);
+        currentSoundFile.play();
+        currentlyPlayingCell.html(pauseButtonTemplate);
+    } else {
+        $('.main-controls .play-pause').html(playerBarPlayButton);
+        currentSoundFile.pause();
+        currentlyPlayingCell.html(playButtonTemplate);
+    }
+};
+
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
@@ -164,9 +178,11 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPause = $('.main-controls .play-pause');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPause.click(togglePlayFromPlayerBar);
 });
